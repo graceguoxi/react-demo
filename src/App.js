8f1cdb3e5144716d509abb4387e31cf09817e906
Grace Guo <guoxi2005@hotmail.com>
2022-11-22 19:29:11 +1100

    login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import MuiTable from "./components/MuiTable";
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './components/Login'
+import MuiTable from './components/MuiTable'
 import SearchAppBar from './components/SearchAppBar'
 import SortTable from './components/SortTable'
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+import { Details } from '@mui/icons-material'
 
 function App() {
   const [searchKeyWord, setSearchKeyWord] = useState('')
 
+  const auth = localStorage.getItem('react-demo-token')
+  let user = {}
+  if (auth) {
+    user = JSON.parse(localStorage.getItem('react-demo-user'))
+}
+  const logout = () => {
+    localStorage.removeItem('react-demo-token')
+    localStorage.removeItem('react-demo-user')
+    setTimeout(() => {
+      window.location.reload()
+    }, 1000)
+  }
+
   return (
     <>
       <SearchAppBar keyWord={searchKeyWord} onSearch={setSearchKeyWord} />
-     
+
       <main>
-        <SortTable searchKeyWord={searchKeyWord} />
+        <Router>
+          <Routes>
+            {auth ? (
+              <Route path='/' element={<SortTable searchKeyWord={searchKeyWord} />} />
+            ) : (
+              <Route path='/' element={<Login />} />
+            )}
+
+            <Route path='login' element={auth ? <Navigate to='/' /> : <Login />} />
+          </Routes>
+        </Router>
+        
+        {
+          auth && <button onClick={logout}>Log out</button>
+        }
+        {
+          user.email
+        }
       </main>
     </>
   )
 }
 
-export default App;
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { Avatar, Button, Checkbox, Grid, Link, Paper, TextField, Typography } from "@mui/material"
+import FormControlLabel from '@mui/material/FormControlLabel';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useState } from "react";
+
+const Login = () => {
+  const [inputs, setInputs] = useState({})
+
+  const paperStyle={padding: 20, height:'70vh', width:380, margin:'20px auto'}
+  const avatarStyle={backgroundColor:'#33cba5'}
+  const btnstyle={margin: '20px 0'}
+
+  const handleChange = (e) => {
+    setInputs(prevState => ({
+      ...prevState,
+      [e.target.name] : e.target.value
+    })) 
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(inputs)
+
+    axios.post('https://app.spiritx.co.nz/api/login', inputs)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('react-demo-token', res.data.token.token)
+        localStorage.setItem('react-demo-user', JSON.stringify(res.data.user))
+
+        setTimeout(() => {
+          window.location.reload()
+        }, 1000)
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <div>
+      <Grid>
+        <Paper elevation={10} style={paperStyle}>
+          <Grid align='center'>
+            <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+            <h2>Sign In</h2>
+          </Grid>
+
+          <form onSubmit={handleSubmit}>
+            <TextField onChange={handleChange} name="email" label="Email" variant="standard" placeholder="Enter your email" fullWidth required />
+            <TextField onChange={handleChange} name="password" label="Password" variant="standard" placeholder="Enter your password" type="password" fullWidth required />
+
+            <Button type='submit' color="primary" variant="contained" style={btnstyle} fullWidth>
+              Sign in
+            </Button>
+          </form>
+        </Paper>
+      </Grid>
+    </div>
+  )
+}
+export default Login
