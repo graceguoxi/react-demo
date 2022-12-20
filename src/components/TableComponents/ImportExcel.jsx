d894ff1b84fa89d7703b3cd53dc3aee5234fb6ee
Grace Guo <guoxi2005@hotmail.com>
2022-12-20 22:59:49 +1100

    Function completion

--- src/components/TableComponents/ImportExcel.jsx
+++ src/components/TableComponents/ImportExcel.jsx
@@ -4,25 +4,32 @@ import * as XLSX from 'xlsx'
 import { Button, IconButton } from '@mui/material'
 import { useState } from 'react'
 
-const ImportExcel = ({ products, setProducts, setMessage }) => {
-
-  const handleRequestImport = e => {
+const ImportExcel = ({
+  products,
+  setProducts,
+  setMessage
+}) => {
+  const handleRequestImport = (e) => {
     const uploadedFile = e.target.files[0]
     const fileReader = new FileReader()
-  
 
     fileReader.readAsArrayBuffer(uploadedFile)
     fileReader.onload = (e) => {
       const bufferArray = e.target.result
-      const workbook = XLSX.read(bufferArray, { type: 'buffer' })
-      const data = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 })
+      const workbook = XLSX.read(bufferArray, {
+        type: 'buffer'
+      })
+      const data = XLSX.utils.sheet_to_json(
+        workbook.Sheets[workbook.SheetNames[0]],
+        { header: 1 }
+      )
       const SliceRows = data.slice(1).map((r) =>
         r.reduce((acc, x, i) => {
           acc[data[0][i]] = x
           return acc
         }, {})
       )
-        console.log(SliceRows)
+      console.log(SliceRows)
 
       setProducts(
         SliceRows.map((pro) => pro),
